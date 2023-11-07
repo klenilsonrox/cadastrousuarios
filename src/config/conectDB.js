46653288df4de4d374be30e81de1fa5c2c99cfe4
klenilsonrox <klenilsoncoc@gmail.com>
2023-11-07 02:56:54 -0300

    asd

--- src/config/conectDB.js
+++ src/config/conectDB.js
@@ -7,11 +7,15 @@ const user =process.env.DB_USER
 const password = process.env.DB_PASSWORD
 
 const mongoURL=`mongodb+srv://${user}:${password}@cadastrodeusuarios.bjkg7tg.mongodb.net/`
+const options = {
+    useUnifiedTopology:true,
+    useNewUrlParser:true
+}
 
 
 export const connectDB = async ()=>{
     try{
-        await mongoose.connect(mongoURL)
+        await mongoose.connect(mongoURL,options)
         console.log("conectado ao mongoDB com sucesso")
     }catch(error){
         console.error("houve um erro", error)
